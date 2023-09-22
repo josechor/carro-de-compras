ac8a29b955631d10ff253a16e5066fddcd809cfe
josechor <joserrapaz5@gmail.com>
2023-09-22 13:33:07 +0200

    Carro de compra

---
+++ src/components/Tittle.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+const styles = {
+    titulo: {
+        marginBottom: '30px'
+    }
+}
+
+class Tittle extends Component {
+  render() {
+    return <h1 style={styles.titulo}>Tienda</h1>;
+  }
+}
+
+export default Tittle;
